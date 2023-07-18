3f26064d7cced532dd32ae63cecdadfc6cf0e29b
André Lagacione <lagacioneandre@gmail.com>
2023-07-17 21:10:18 -0300

    create fornituries

--- Abstract Factory/src/BuildForniture.ts
+++ Abstract Factory/src/BuildForniture.ts
@@ -2,12 +2,18 @@ import { FornitureType } from "./enums/forniture-type.enum";
 import { ArtdecoFornitureFactory } from "./forniture/ArtdecoFornitureFactory";
 import { ModernFornitureFactory } from "./forniture/ModernFornitureFactory";
 import { VictorianFornitureFactory } from "./forniture/VictorianFornitureFactory";
+import { IFornitureFactory } from "./forniture/interfaces/forniture-factory.interface";
 
 export class BuildForniture {
 
-    constructor() {}
+    constructor() {
+        const factory = this.build(FornitureType.ARTDECO);
+        const chair = factory.createChair();
+        const sofa = factory.createSofa();
+        const table = factory.createTable();
+    }
 
-    build(fornitureType: FornitureType) {
+    build(fornitureType: FornitureType): IFornitureFactory {
         if (fornitureType === FornitureType.MODERN) {
             return new ModernFornitureFactory();
         } else if (fornitureType === FornitureType.VICORIAN) {
@@ -15,5 +21,7 @@ export class BuildForniture {
         } else if (fornitureType === FornitureType.ARTDECO) {
             return new ArtdecoFornitureFactory();
         }
+
+        return new ArtdecoFornitureFactory();
     }
 }
